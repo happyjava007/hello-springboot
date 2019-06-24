c25facdd2f8b3aef1274deca5aa480c1bbaea291
Loger <chenle199509@qq.com>
2019-06-24 22:45:47 +0800

    a

---
+++ transaction-propagation/src/main/java/cn/happyjava/transactionpropagation/TransactionPropagationApplication.java
@@ -0,0 +1,13 @@
+package cn.happyjava.transactionpropagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionPropagationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionPropagationApplication.class, args);
+    }
+
+}
