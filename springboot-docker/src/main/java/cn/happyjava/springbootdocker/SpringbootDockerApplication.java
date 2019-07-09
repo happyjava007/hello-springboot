67423a50984a603de97df0a388971462d2aad016
Loger <chenle199509@qq.com>
2019-07-09 19:08:24 +0800

    springboot-docker'

---
+++ springboot-docker/src/main/java/cn/happyjava/springbootdocker/SpringbootDockerApplication.java
@@ -0,0 +1,21 @@
+package cn.happyjava.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerApplication.class, args);
+    }
+
+    @GetMapping(value = "/test")
+    public Object test() {
+        return "Hello SpringBoot with Docker!";
+    }
+
+}
