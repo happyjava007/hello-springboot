5008db2188219b978e7b821fb66153ad9f0d1b28
Loger <chenle199509@qq.com>
2019-07-24 18:59:39 +0800

    spring cache redis

---
+++ springboot-cache-redis/src/main/java/cn/happyjava/hello/springboot/springbootcacheredis/SpringbootCacheRedisApplication.java
@@ -0,0 +1,13 @@
+package cn.happyjava.hello.springboot.springbootcacheredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCacheRedisApplication.class, args);
+    }
+
+}
